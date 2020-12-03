d3f5af446b9c888e57ae1ea1b367a44e6c67d618
Oluwaseyi Kareem <okareem@Oluwaseyis-MBP.hitronhub.home>
2020-12-03 11:12:24 -0600

    Day1 and Day2 of AdventOfCode

---
+++ Day_2/Part1.sql
@@ -0,0 +1,6 @@
+select count(*) from dbo.AdventDay2
+WHERE LEN(column3) - LEN(REPLACE(column3, SUBSTRING(column2,1,1),'')) >= CAST(LEFT(column1,CHARINDEX('-',column1)-1) AS INT) --Left Side Always Valid 
+ AND LEN(column3) - LEN(REPLACE(column3, SUBSTRING(column2,1,1), '')) <= CASE
+  WHEN LEN(column1) = 4 THEN CAST(RIGHT(column1,CHARINDEX('-',column1)) AS INT) ELSE  -- depeding on format of right side i.e (1-5), (2-10), (10-12),
+                                                                                      -- indexing would differ, so one will have to check
+  CAST(RIGHT(column1,CHARINDEX('-',column1)-1) AS INT) END 
