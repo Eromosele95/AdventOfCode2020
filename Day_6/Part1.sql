bda4b55b53d59e044fe7a530f67f89b9f7cc2fe1
Oluwaseyi Kareem <okareem@Oluwaseyis-MBP.hitronhub.home>
2020-12-09 21:41:28 -0600

    Added Tally Table Attempt for Part1

--- Day_6/Part1.sql
+++ Day_6/Part1.sql
@@ -23,3 +23,79 @@ CREATE FUNCTION [dbo].[RemoveDupsClean](@str VARCHAR(500))
         END
 
 select SUM(LEN(dbo.RemoveDupsClean(column1))) from dbo.AdventDay6
+
+
+
+-- CREATE FUNCTION dbo.RemoveDupChars (@S VARCHAR(8000))
+-- RETURNS TABLE
+-- 	WITH SCHEMABINDING
+--
+-- RETURN
+-- WITH TallyTable(N) AS (
+-- 		SELECT ROW_NUMBER() OVER (
+-- 				ORDER BY (
+-- 						SELECT NULL
+-- 						)
+-- 				)
+-- 		FROM (
+-- 			VALUES (0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0)
+-- 			) a(n)
+-- 		CROSS JOIN (
+-- 			VALUES (0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0),
+-- 				(0)
+-- 			) c(n)
+-- 		)
+--
+-- SELECT OriginalString = @S,
+-- 	NewString = (
+-- 		SELECT '' + s
+-- 		FROM (
+-- 			SELECT n,
+-- 				s
+-- 			FROM (
+-- 				-- Choose First Element
+-- 				SELECT n = 1,
+-- 					s = LEFT(@S, 1)
+--
+-- 				UNION ALL
+--
+-- 				SELECT N,
+-- 					CASE
+-- 						WHEN RIGHT(@S, LEN(@S) - N) NOT LIKE CONCAT (
+-- 								'%',
+-- 								SUBSTRING(@S, N, 1),
+-- 								'%'
+-- 								)
+-- 							--If the right portion of the string is not like the left character being added
+-- 							AND SUBSTRING(@S, N - 1, 1) <> SUBSTRING(@S, N, 1)
+-- 							-- If the character being added is not equal to the successive
+-- 							THEN SUBSTRING(@S, N, 1)
+-- 						END
+-- 				FROM TallyTable
+-- 				WHERE N BETWEEN 2
+-- 						AND LEN(@S)
+-- 				) a
+-- 			--Filter NULLS
+-- 			WHERE s IS NOT NULL
+-- 			) b
+-- 		ORDER BY n
+-- 		FOR XML PATH(''),
+-- 			TYPE
+-- 		).value('.', 'VARCHAR(8000)');
